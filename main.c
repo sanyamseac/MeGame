d1395c101b98ce0f58ffacdb2bf23bfb567514c4
orbi8r <orbi8r+github@gmail.com>
2024-10-26 22:07:51 +0530

    primary

--- main.c
+++ main.c
@@ -1,12 +1,108 @@
 #include "raylib.h"
 #include "play.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
 
-int main(void)
-{
+#define PORT 8080
+#define MAX_CLIENTS 2
+
+typedef struct {
+    int socket;
+    struct sockaddr_in address;
+    int addr_len;
+    int index;
+} client_t;
+
+client_t *clients[MAX_CLIENTS];
+pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void broadcast(char *message, int exclude_index) {
+    pthread_mutex_lock(&clients_mutex);
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        if (clients[i] && clients[i]->index != exclude_index) {
+            send(clients[i]->socket, message, strlen(message), 0);
+        }
+    }
+    pthread_mutex_unlock(&clients_mutex);
+}
+
+void *handle_client(void *arg) {
+    char buffer[1024];
+    client_t *cli = (client_t *)arg;
+
+    while (1) {
+        int bytes_received = recv(cli->socket, buffer, sizeof(buffer), 0);
+        if (bytes_received <= 0) {
+            close(cli->socket);
+            pthread_mutex_lock(&clients_mutex);
+            clients[cli->index] = NULL;
+            pthread_mutex_unlock(&clients_mutex);
+            free(cli);
+            break;
+        } else {
+            buffer[bytes_received] = '\0';
+            broadcast(buffer, cli->index);
+        }
+    }
+
+    return NULL;
+}
+
+void start_server() {
+    int server_socket, new_socket;
+    struct sockaddr_in server_addr, new_addr;
+    socklen_t addr_size;
+    pthread_t tid;
+
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    listen(server_socket, MAX_CLIENTS);
+
+    printf("Server listening on port %d\n", PORT);
+
+    while (1) {
+        addr_size = sizeof(new_addr);
+        new_socket = accept(server_socket, (struct sockaddr *)&new_addr, &addr_size);
+
+        if (new_socket < 0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        pthread_mutex_lock(&clients_mutex);
+        for (int i = 0; i < MAX_CLIENTS; ++i) {
+            if (!clients[i]) {
+                client_t *cli = (client_t *)malloc(sizeof(client_t));
+                cli->socket = new_socket;
+                cli->address = new_addr;
+                cli->addr_len = addr_size;
+                cli->index = i;
+                clients[i] = cli;
+                pthread_create(&tid, NULL, handle_client, (void *)cli);
+                break;
+            }
+        }
+        pthread_mutex_unlock(&clients_mutex);
+    }
+}
+
+int main(void) {
+    // Start the server in a separate thread
+    pthread_t server_thread;
+    pthread_create(&server_thread, NULL, (void *)start_server, NULL);
+
     // Initialization
-    const int screenWidth = 720;
-    const int screenHeight = 720;
-
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
+    
     InitWindow(screenWidth, screenHeight, "Main Menu");
 
     SetTargetFPS(60); // Set our game to run at 60 frames-per-second
--- play.c
+++ play.c
@@ -1,35 +1,103 @@
 #include "raylib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
 
-void RunGame(void)
-{
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 8080
+#define MAX_PLAYERS 10
+
+typedef struct Player {
+    Vector2 position;
+    Color color;
+    int upKey;
+    int downKey;
+    int leftKey;
+    int rightKey;
+} Player;
+
+int client_socket;
+Player localPlayer;
+Player remotePlayers[MAX_PLAYERS];
+int numRemotePlayers = 0;
+
+void *receive_data(void *arg) {
+    char buffer[1024];
+    while (1) {
+        int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
+        if (bytes_received > 0) {
+            buffer[bytes_received] = '\0';
+            int playerIndex;
+            float posX, posY;
+            sscanf(buffer, "%d %f %f", &playerIndex, &posX, &posY);
+            if (playerIndex >= 0 && playerIndex < MAX_PLAYERS) {
+                remotePlayers[playerIndex].position.x = posX;
+                remotePlayers[playerIndex].position.y = posY;
+            }
+        }
+    }
+    return NULL;
+}
+
+void RunGame(void) {
     // Initialization
-    const int screenWidth = 720;
-    const int screenHeight = 720;
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - multiplayer");
 
-    Vector2 playerPosition = { (float)screenWidth/2, (float)screenHeight/2 };
+    localPlayer = (Player){{(float)screenWidth / 4, (float)screenHeight / 2}, RED, KEY_W, KEY_S, KEY_A, KEY_D};
+    for (int i = 0; i < MAX_PLAYERS; i++) {
+        remotePlayers[i] = (Player){{0, 0}, BLUE, 0, 0, 0, 0};
+    }
+
     SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+
+    // Connect to server
+    struct sockaddr_in server_addr;
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr);
+
+    connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+    pthread_t receive_thread;
+    pthread_create(&receive_thread, NULL, receive_data, NULL);
 
     // Main game loop
     while (!WindowShouldClose()) // Detect window close button or ESC key
     {
         // Update
-        if (IsKeyDown(KEY_RIGHT)) playerPosition.x += 2.0f;
-        if (IsKeyDown(KEY_LEFT)) playerPosition.x -= 2.0f;
-        if (IsKeyDown(KEY_UP)) playerPosition.y -= 2.0f;
-        if (IsKeyDown(KEY_DOWN)) playerPosition.y += 2.0f;
+        if (IsKeyDown(localPlayer.rightKey)) localPlayer.position.x += 2.0f;
+        if (IsKeyDown(localPlayer.leftKey)) localPlayer.position.x -= 2.0f;
+        if (IsKeyDown(localPlayer.upKey)) localPlayer.position.y -= 2.0f;
+        if (IsKeyDown(localPlayer.downKey)) localPlayer.position.y += 2.0f;
+
+        // Send player position to server
+        char buffer[1024];
+        sprintf(buffer, "%d %f %f", 0, localPlayer.position.x, localPlayer.position.y); // Assuming local player is player 0
+        send(client_socket, buffer, strlen(buffer), 0);
 
         // Draw
         BeginDrawing();
 
         ClearBackground(DARKGRAY);
 
-        DrawCircleV(playerPosition, 20, RED); // Draw player character as a red circle
+        DrawCircleV(localPlayer.position, 20, localPlayer.color); // Draw local player
+        for (int i = 0; i < MAX_PLAYERS; i++) {
+            if (remotePlayers[i].position.x != 0 || remotePlayers[i].position.y != 0) {
+                DrawCircleV(remotePlayers[i].position, 20, remotePlayers[i].color); // Draw remote players
+            }
+        }
 
         EndDrawing();
     }
 
     // De-Initialization
     CloseWindow(); // Close window and OpenGL context
+    close(client_socket);
 }
